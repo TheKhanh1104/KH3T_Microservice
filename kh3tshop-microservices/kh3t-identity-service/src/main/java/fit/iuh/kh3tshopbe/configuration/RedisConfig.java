944d5981dcf8a3c152a192dcb8a1a267d14cc58b
PhamHinh2004 <20102004hinh@gmail.com>
2026-06-01 11:22:27 +0700

    finish project kh3t shop

--- kh3tshop-microservices/kh3t-identity-service/src/main/java/fit/iuh/kh3tshopbe/configuration/RedisConfig.java
+++ kh3tshop-microservices/kh3t-identity-service/src/main/java/fit/iuh/kh3tshopbe/configuration/RedisConfig.java
@@ -2,6 +2,7 @@ package fit.iuh.kh3tshopbe.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -10,8 +11,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.host:localhost}") String host,
+            @Value("${spring.data.redis.port:6379}") int port) {
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
